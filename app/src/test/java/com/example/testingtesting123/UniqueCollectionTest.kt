e442f5a80a9187578937af3457d85c8c81dd672b
Timothy <timkhumpan@gmail.com>
2024-12-02 15:15:34 -0500

    complete tests

--- app/src/test/java/com/example/testingtesting123/UniqueCollectionTest.kt
+++ app/src/test/java/com/example/testingtesting123/UniqueCollectionTest.kt
@@ -1,5 +1,6 @@
 package com.example.testingtesting123
 
+import junit.framework.TestCase.assertEquals
 import org.junit.Before
 import org.junit.Test
 
@@ -16,13 +17,23 @@ class UniqueCollectionTest {
     // TODO 1: Write a test to ensure items can be added to the collection
     @Test
     fun addAnItem() {
+        val item = Item("testItem")
+        collection.addItem(item)
 
+        assertEquals(1, collection.size())
     }
 
     // TODO 2: Write a test to ensure that only unique items can be added to the collection
     // Uniqueness is determined by the Item.name property, which is set via the constructor
     @Test
     fun addUniqueItem() {
+        val item1 = Item("testItem")
+        val item2 = Item("testItem")
+
+        collection.addItem(item1)
+        collection.addItem(item2)
+
+        assertEquals(1, collection.size())
 
     }
 
